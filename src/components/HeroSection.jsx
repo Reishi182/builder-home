f52ef6948946171f8e346ef06519ebca0833cb4f
Reishi18 <garytan231@gmail.com>
2024-05-15 20:27:08 +0700

    fix

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -9,16 +9,16 @@ export default function HeroSection() {
   const [search, setSearch] = useState("");
   return (
     <section className="bg-[url(/img/bgHero.png)] bg-center bg-cover px-20 py-24 ">
-      <div className="flex ">
-        <div className=" w-full z-0 ">
-          <h1 className="text-[#5E8451] text-[6rem] font-semibold text-wrap ">
+      <div className="flex  ">
+        <div className=" w-full z-0 flex flex-col lg:items-start items-center ">
+          <h1 className="text-[#5E8451] sm:text-[6rem] text-[4.6rem] lg:text-start text-center font-semibold text-wrap ">
             <span className="block">Temukan Desain</span>
             <span className="block">Rumah Impianmu!</span>
           </h1>
-          <span className="text-[2.3rem] block">
+          <span className="text-[2.3rem] block lg:text-start text-center">
             Bangun rumah lebih mudah bersama Builder Home
           </span>
-          <div className="flex items-center mt-10">
+          <div className="flex items-center justify-center lg:justify-normal mt-10 sm:w-full w-[40rem]">
             <input
               className="w-[43rem] py-6 px-8 rounded-l-lg text-[1.5rem]"
               placeholder="Cari Desain Rumah"
@@ -38,7 +38,7 @@ export default function HeroSection() {
               Rumah Tropis
             </Badge>
           </h1>
-          <div className="bg-white flex mt-16 items-center justify-center px-10 space-x-7 py-12 w-[55rem] rounded-xl ">
+          <div className="bg-white flex mt-16 items-center justify-center px-10 space-x-7 py-12 sm:w-[55rem] w-[44rem] rounded-xl ">
             <TabName value={999} name="Koleksi Desain" icon={<FaHouse />} />
             <TabName
               value={1000}
--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 export default function Icon({ children }) {
   return (
-    <span className="block rounded-full px-2 py-2 bg-white">{children}</span>
+    <button className="block rounded-full px-2 py-2 bg-white">
+      {children}
+    </button>
   );
 }
